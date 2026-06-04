da5e4b0916c07226580a46ffa49c2de031caccb4
Bex Edmondson <bexedmondson@gmail.com>
2026-06-04 19:03:49 +0100

    editing layout

--- DropSlot.gd
+++ DropSlot.gd
@@ -3,12 +3,13 @@ class_name DropSlot extends Control
 signal tile_changed(slot: DropSlot)
 
 @export var circle: Control
+@export var container: Container
 
 var slotTile: DropTile
 
 func dragged_away(tile: DropTile) -> void:
 	tile.dragged_away.disconnect(dragged_away)
-	remove_child(tile)
+	container.remove_child(tile)
 	slotTile = null
 	tile_changed.emit(self)
 
@@ -17,7 +18,7 @@ func setup_tile(tile: DropTile) -> void:
 
 func add_tile(tile: DropTile) -> void:
 	tile.dragged_away.connect(dragged_away)
-	add_child(tile)
+	container.add_child(tile)
 
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	return slotTile == null
@@ -38,7 +39,7 @@ var tween: Tween
 func highlight():
 	if (tween == null || !tween.is_valid()):
 		tween = create_tween()
-		tween.tween_property(circle, "modulate:a", 0, 0.5).from(1.0)
+		tween.tween_property(circle, "modulate:a", 0, 0.8).from(1.0)
 	if (tween.is_running()):
 		tween.stop()
 	tween.play()
