8b2c9ff3b670cd7c5fb4d22e73660b3f354ecc72
Bex Edmondson <bexedmondson@gmail.com>
2026-07-23 18:00:57 +0100

    tweaking various layout bits

--- scripts/AspectRatioTweaker.gd
+++ scripts/AspectRatioTweaker.gd
@@ -1,13 +1,36 @@
 extends Node
 
 @export var wordListScroller : ScrollContainer
+@export var leftGridSpacer : Control
+@export var rightGridSpacer : Control
+
+@export var onOnlyWhenHorizontal : Array[Control]
+
+var most_recent_check_vertical : bool = false
+var leftSpacerFlagsVertical = Control.SizeFlags.SIZE_FILL
+var rightSpacerFlagsVertical = (Control.SizeFlags.SIZE_EXPAND & Control.SizeFlags.SIZE_SHRINK_END)
+var spacerFlagsHorizontal = (Control.SizeFlags.SIZE_EXPAND & Control.SizeFlags.SIZE_SHRINK_CENTER)
 
 func _on_ready():
-	get_viewport().size_changed.connect(tweak)
+	var screen = get_viewport().get_screen_transform()
+	most_recent_check_vertical = screen.x * 1.1 < screen.y
 
 func tweak():
-	var screen = get_viewport().get_screen_transform()
-	if (screen.x * 1.1 < screen.y):
-		wordListScroller.vertical_scroll_mode = ScrollContainer.SCROLL_MODE_DISABLED
-	else:
-		wordListScroller.vertical_scroll_mode = ScrollContainer.SCROLL_MODE_RESERVE
+	var screen = get_tree().get_root().size
+	var is_vertical = screen.x * 1.1 < screen.y
+	
+	if most_recent_check_vertical == is_vertical:
+		return
+	
+	most_recent_check_vertical = is_vertical
+	
+	wordListScroller.vertical_scroll_mode = ScrollContainer.SCROLL_MODE_DISABLED if is_vertical else ScrollContainer.SCROLL_MODE_RESERVE
+	
+	leftGridSpacer.set_h_size_flags(leftSpacerFlagsVertical if is_vertical else spacerFlagsHorizontal)
+	rightGridSpacer.set_h_size_flags(rightSpacerFlagsVertical if is_vertical else spacerFlagsHorizontal)
+	
+	_toggle_items()
+
+func _toggle_items():
+	for control in onOnlyWhenHorizontal:
+		control.visible = !most_recent_check_vertical
