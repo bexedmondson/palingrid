62d75d3b0b400abecf9217c2a312ddcfb4c90803
Bex Edmondson <bexedmondson@gmail.com>
2026-08-11 23:45:35 +0100

    names can be 16 letters long now :)

--- scripts/NameChangeHandler.gd
+++ scripts/NameChangeHandler.gd
@@ -13,7 +13,7 @@ signal on_closed
 @export var back_button : Button
 
 const MIN_NAME_LENGTH: int = 3
-const MAX_NAME_LENGTH: int = 12
+const MAX_NAME_LENGTH: int = 16
 
 const valid_chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 					'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
