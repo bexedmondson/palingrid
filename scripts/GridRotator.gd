fc458c7f52fab4e2860d0c7d644ddfd63cfcdae5
Bex Edmondson <bexedmondson@gmail.com>
2026-08-14 22:31:36 +0100

    rotate tween animation :)

--- scripts/GridRotator.gd
+++ scripts/GridRotator.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal end_of_frame
+
 @export var grid : Grid
 
 var rotatedSlotIndexMap = [
@@ -12,10 +14,16 @@ var rotatedSlotIndexMap = [
 
 # TODO stop the grid from rechecking for words until all tiles have been removed and replaced
 
+var _rotate_tween : Tween
+
 func rotate():
+	if _rotate_tween != null && _rotate_tween.is_valid():
+		return
+	
 	var cells = grid.slots
 	
-	var tileToCellIndexMap = {}
+	var tileToCellIndexMap : Dictionary[DropTile, int] = {}
+	var tileToOriginalGlobalPositionMap : Dictionary[DropTile, Vector2] = {}
 	
 	for index in cells.size():
 		var cell = cells[index]
@@ -23,6 +31,7 @@ func rotate():
 			continue
 		
 		tileToCellIndexMap[cell.slotTile] = index
+		tileToOriginalGlobalPositionMap[cell.slotTile] = cell.slotTile.global_position
 		cell.remove_tile(cell.slotTile)
 	
 	for tile in tileToCellIndexMap:
@@ -30,3 +39,22 @@ func rotate():
 		var targetCellIndex = rotatedSlotIndexMap[currentCellIndex]
 		var targetCell = cells[targetCellIndex]
 		targetCell.add_tile(tile)
+
+	on_end_of_frame.call_deferred()
+	await end_of_frame
+
+	_rotate_tween = create_tween()
+	_rotate_tween.set_parallel(true)
+	_rotate_tween.set_trans(Tween.TRANS_SINE)
+	_rotate_tween.set_ease(Tween.EASE_IN_OUT)
+	
+	for tile in tileToOriginalGlobalPositionMap:
+		tile.offset_transform_enabled = true
+		tile.offset_transform_position = tileToOriginalGlobalPositionMap[tile] - tile.global_position
+		_rotate_tween.tween_property(tile, "offset_transform_position", Vector2.ZERO, 0.6)
+	
+	_rotate_tween.play()
+
+	
+func on_end_of_frame():
+	end_of_frame.emit()
