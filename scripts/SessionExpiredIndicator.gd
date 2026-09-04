05986e046bfb16a02ff82cd168d61af5ca62b4c9
Bex Edmondson <bexedmondson@gmail.com>
2026-09-04 16:40:13 +0100

    attempting to correctly disable the session expired indicator after logging in

--- scripts/SessionExpiredIndicator.gd
+++ scripts/SessionExpiredIndicator.gd
@@ -10,7 +10,9 @@ func _on_session_expired():
 	self.visible = true
 	CheddaBoards.session_expired.disconnect(_on_session_expired)
 	CheddaBoards.login_success.connect(_on_logged_in)
+	CheddaBoards.account_upgraded.connect(_on_logged_in)
 
 func _on_logged_in():
 	CheddaBoards.login_success.disconnect(_on_logged_in)
+	CheddaBoards.account_upgraded.disconnect(_on_logged_in)
 	self.visible = false
